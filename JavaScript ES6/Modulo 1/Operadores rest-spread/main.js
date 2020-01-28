f2d07dd269b5f19cf79b18053866a9b84ba39a25
Mikael Arthur <mikael.atala@gmail.com>
2020-01-28 15:38:32 -0300

    codigo da aula do operadores rest e spread do modulo 1, javascript ES6

--- JavaScript ES6/Modulo 1/Operadores rest-spread/main.js
+++ JavaScript ES6/Modulo 1/Operadores rest-spread/main.js
@@ -10,3 +10,30 @@ const { nome, ...resto } =  usuario;
 
 console.log(nome);
 console.log(resto);
+
+function soma1(a, b, c) {
+    return a + b + c;
+}
+console.log(1,3,5);
+
+function soma2( ...params) {
+    return params;
+}
+
+console.log(soma(1,2,4,5,7,8,9));
+
+// SPREAD
+
+const arr1 = [1,3,5];
+const arr2 = [2,4,6];
+
+const arr3 = [ ...arr1, ...arr2];
+
+const usuario1 = {
+    nome: 'André',
+    idade: 23,
+    empresa: 'Uabit'
+};
+
+const usuario2 = { ...usuario1, nome: 'Mikael'};
+console.log(usuario2);
